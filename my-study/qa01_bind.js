dd2846dd24634af2ab6d6095bfd3d7b5811cbffe
lqqojin <lqqojin@gmail.com>
2019-09-22 13:37:49 +0900

    삭제 commit

--- my-study/qa01_bind.js
+++ my-study/qa01_bind.js
@@ -1,16 +1,5 @@
 // 설명 필요
 
-// function sum(num) {
-// 	console.log(this);
-// 	return num + this.num1 + this.num2;
-// }
-//
-// var myObj = {num1:20, num2: 3};
-//
-// var customSum = sum.bind(myObj);
-//
-// console.log(customSum(5));
-
 class Point {
 	constructor(x, y) {
 		this.x = x;
